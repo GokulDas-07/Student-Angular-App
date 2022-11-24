c19289232bda2bd2b3b5abc7436ef99ec853fa90
Gokul Das M <Gokul@NTP-LAP-723.chn.nestgroup.net>
2022-11-24 15:12:37 +0530

    delete student added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StudentDeleteComponent } from './student-delete/student-delete.component';
 import { StudentEntryComponent } from './student-entry/student-entry.component';
 import { StudentSearchComponent } from './student-search/student-search.component';
 
 const routes: Routes = [
   {path:"",component:StudentEntryComponent},
-  {path:"search",component:StudentSearchComponent}
+  {path:"search",component:StudentSearchComponent},
+  {path:"delete",component:StudentDeleteComponent}
 ];
 
 @NgModule({
